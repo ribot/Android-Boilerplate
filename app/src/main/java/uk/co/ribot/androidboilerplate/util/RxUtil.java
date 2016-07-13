04c76c94387730a27cb6b9364f66f0a94cae32aa
Andrew <jydimir@gmail.com>
2016-07-13 14:35:44 +0300

    subscription leaks removed

--- app/src/main/java/uk/co/ribot/androidboilerplate/util/RxUtil.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/util/RxUtil.java
@@ -5,7 +5,7 @@ import rx.Subscription;
 public class RxUtil {
 
     public static void unsubscribe(Subscription subscription) {
-        if((subscription != null) && (!subscription.isUnsubscribed())) {
+        if (subscription != null && !subscription.isUnsubscribed()) {
             subscription.unsubscribe();
         }
     }
