79b3aa1e0618267eba6315cfb89315bc0e7d61c6
Vinícius Soares <squimer@gmail.com>
2017-06-06 01:11:47 -0300

    Remove RxJava 1!!!

--- app/src/main/java/uk/co/ribot/androidboilerplate/util/RxUtil.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/util/RxUtil.java
@@ -1,15 +1,8 @@
 package uk.co.ribot.androidboilerplate.util;
 
 import io.reactivex.disposables.Disposable;
-import rx.Subscription;
 
 public class RxUtil {
-
-    public static void unsubscribe(Subscription subscription) {
-        if (subscription != null && !subscription.isUnsubscribed()) {
-            subscription.unsubscribe();
-        }
-    }
 
     public static void dispose(Disposable disposable) {
         if (disposable != null && !disposable.isDisposed()) {
